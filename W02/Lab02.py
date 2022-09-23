9a8bbf5a63d824a1e74a73b5042f22275dbbdab5
garrettbadger <garrett.badger.id@gmail.com>
2022-09-23 13:02:11 -0600

    finished week 2 and added a file to mess with discussion board prompts

---
+++ W02/Lab02.py
@@ -0,0 +1,43 @@
+# 1. Name:
+#      Garrett Badger
+# 2. Assignment Name:
+#      Lab 02: Authentication
+# 3. Assignment Description:
+#      This program is meant to read a json object into python and then using the json authenticate users who present valid usernames and passwords
+# 4. What was the hardest part? Be as specific as possible.
+#      Remebering how to manipulate json
+# 5. How long did it take for you to complete the assignment?
+#      3 hours
+import json
+try:
+    x = open('C:/Users/flipp/CSE-130/W02/Lab02.json')
+
+except:
+    print("Unable to open file Lab02.json")
+
+data = x.read()
+
+jsondata = json.loads(data)
+
+usernames = jsondata['username']
+passwords = jsondata['password']
+
+username = input("Please enter a username: ")
+password = input("Please enter a password: ")
+
+u_location = -1
+p_location = -1
+
+for i in range(len(usernames)):
+    if (usernames[i] == username):
+        u_location = i
+    
+for i in range(len(passwords)):
+    if (passwords[i] == password):
+        p_location = i
+    
+
+if(p_location == u_location):
+    print("You are authenticated!")
+else:
+    print("You are not authorized to use this system.")
---
+++ discussionboard.py
@@ -0,0 +1,7 @@
+text = """This is some \n multi-line text."""
+
+letter = text[0]
+
+substring = text[2:6]
+
+list = text.split("u")
