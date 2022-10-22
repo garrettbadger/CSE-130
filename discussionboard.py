048c91041b186bfabcaa3ed039ed580f33adb77b
garrettbadger <garrett.badger.id@gmail.com>
2022-10-22 12:28:38 -0600

    discussion board

--- discussionboard.py
+++ discussionboard.py
@@ -7,17 +7,22 @@
 # list = text.split("u")
 
 
-array = ['22', '9', '45', '223', '308', '762']
+# array = ['22', '9', '45', '223', '308', '762']
 
-search_value = '223'
+# search_value = '223'
 
-array_elements = len(array)
+# array_elements = len(array)
 
-while array_elements > 1:
-    if (array[array_elements-1] == search_value):
-        print("Found search item!")
-        break
-    else:
-        array_elements -= 1
-if(array_elements == 1):
-    print("item not found!")
+# while array_elements > 1:
+#     if (array[array_elements-1] == search_value):
+#         print("Found search item!")
+#         break
+#     else:
+#         array_elements -= 1
+# if(array_elements == 1):
+#     print("item not found!")
+
+if 'a' < 'b':
+    print('true')
+else:
+    print('false')
