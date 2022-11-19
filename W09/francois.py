99c65f499bb2b96b9f41b1a992f1938a1e298c74
garrettbadger <garrett.badger.id@gmail.com>
2022-11-19 11:29:48 -0700

    upated francois

--- W09/francois.py
+++ W09/francois.py
@@ -1,10 +1,16 @@
-master_francois = [2, 1]
-def francois(num, m_list):
-    if num <= len(m_list):
-        return m_list[num-1]
-    for i in range(2, num):
-        m_list.append(m_list[-1] + m_list[-2])
-    return m_list[-1]        
+
+def francois(num):
+    m_list = [1, 2]
+    if num > 2:
+        for i in range(3, num+1):
+            m_list[i % 2] = m_list[0] + m_list[1]
+    value = m_list[num % 2]
+    return value
+    # if num <= len(m_list):
+    #     return m_list[num-1]
+    # for i in range(2, num):
+    #     m_list.append(m_list[-1] + m_list[-2])
+    # return m_list[-1]        
 
 
 
@@ -12,7 +18,7 @@ def main():
     another = True
     while another:
         count = int(input("Which number would you like? "))
-        print(francois(count, master_francois))
+        print(francois(count))
         Y = input("Do you want to get another number? (y/n) ")
         if Y == 'y':
             another = True
